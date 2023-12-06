fe2dbc827e311b7ebeff320a222e4f2a73d85adf
Paul Vakhnivskyi <pvakhnivskyi@gmail.com>
2023-12-06 12:41:49 +0200

    Update jsonrpsee

--- client/http-client/src/client.rs
+++ client/http-client/src/client.rs
@@ -325,8 +325,10 @@ where
 
 		// NOTE: it's decoded first to `JsonRawValue` and then to `R` below to get
 		// a better error message if `R` couldn't be decoded.
-		let response = ResponseSuccess::try_from(serde_json::from_slice::<Response<&JsonRawValue>>(&body)?)?;
-		tracing::debug!(?response);
+		let json_body = serde_json::from_slice::<Response<&JsonRawValue>>(&body)?;
+		tracing::debug!(?json_body);
+
+		let response = ResponseSuccess::try_from(json_body)?;
 
 		let result = serde_json::from_str(response.result.get()).map_err(Error::ParseError)?;
 
